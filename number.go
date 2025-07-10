cf1e1831b6ce88e015be5cb355070b922b98b3b0
Muhammad Habil Arifin <mr.habilhabil1405@gmail.com>
2025-07-10 17:44:59 +0700

    first

---
+++ number.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+/* di Golang redeclared in this block artinya tidak boleh sama func main didalam folder atau harus ada satu saja
+Untuk menjalakan program di golang go run helloworld aja jika ingin melihat hasilnya aja
+*/
+
+
+func main() {
+	fmt.Println("Satu = 1 ", 1)
+	fmt.Println("Dua = 2 ", 2 )
+	fmt.Println("Tiga koma Lima", 3.5)
+}
---
+++ string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main()  {
+	fmt.Println("Habil")
+	fmt.Println("Muhammad Habil")
+	fmt.Println("Muhammad Habil Arifin")
+
+	fmt.Println(len("Habil"))
+	fmt.Println("Muhammad Habil"[0])
+	fmt.Println("Muhammad Habil Arifin"[1])
+}
