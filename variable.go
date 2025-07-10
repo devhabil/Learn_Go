d44cc4c475ea773eb8f358749597224e9921520e
Muhammad Habil Arifin <mr.habilhabil1405@gmail.com>
2025-07-10 18:42:03 +0700

    Second

---
+++ variable.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	/*
+		Digolang tidak wajib menyebutkan tipe data variablenya
+		:= simbol disamping agar kita tidak menggunakan kata var, ini boleh dipakai jika di deklaris baris pertama aja
+	*/
+	name := "Habil Arifin"
+	fmt.Println(name)
+
+	name = "Muhammad Habil"
+	fmt.Println(name)
+}
