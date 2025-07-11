d7bf78da832626396b4e19c5996365c601646aaa
Muhammad Habil Arifin <mr.habilhabil1405@gmail.com>
2025-07-11 22:16:37 +0700

    third

--- variable.go
+++ variable.go
@@ -7,9 +7,19 @@ func main() {
 		Digolang tidak wajib menyebutkan tipe data variablenya
 		:= simbol disamping agar kita tidak menggunakan kata var, ini boleh dipakai jika di deklaris baris pertama aja
 	*/
-	name := "Habil Arifin"
-	fmt.Println(name)
+	Name := "Habil Arifin"
+	fmt.Println(Name)
 
-	name = "Muhammad Habil"
-	fmt.Println(name)
+	Name = "Muhammad Habil"
+	fmt.Println(Name)
+
+	var(
+		firstName = "Muhammad"
+		middledName ="Habil"
+		lastName = "Arifin"
+	)
+
+	fmt.Println(firstName)
+	fmt.Println(middledName)
+	fmt.Println(lastName)
 }
